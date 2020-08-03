debee650fbc38fe4ae2724e43e8e1a42cb9092c6
goonja gandhi <goonja@goonjas-MacBook-Pro.local>
2020-08-04 00:18:11 +0530

    Folder structure made

--- angular4-complete-guide/src/app/app.module.ts
+++ angular4-complete-guide/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipies/recipe-details/recipe-details.component';
+import { ShopphingListComponent } from './shopphing-list/shopphing-list.component';
+import { ShoppingEditComponent } from './shopphing-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShopphingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ angular4-complete-guide/src/app/shopphing-list/shopphing-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopphing-list',
+  templateUrl: './shopphing-list.component.html',
+  styleUrls: ['./shopphing-list.component.css']
+})
+export class ShopphingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
